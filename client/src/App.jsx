4b620e2e2983bb08b0bf1dbafc9d60e340b6dd7d
terrdv <terrdv7@gmail.com>
2025-09-27 01:38:27 -0700

    Question Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import QuestionPage from './pages/QuestionPage'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/questions" element={<QuestionPage />} />
       </Routes>
     </>
   )
--- client/src/pages/QuestionPage.jsx
+++ client/src/pages/QuestionPage.jsx
@@ -1,3 +1,50 @@
+import { Link } from "react-router-dom";
+import '../css/QuestionPage.css';
+import {useEffect, useState} from "react";
 
 
 
+function QuestionPage() {
+
+    const [questions, setQuestions] = useState([]);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const [answers, setAnswers] = useState({});
+
+    return (
+        <div className="question-bg">
+        <h1 className="question-title">
+            Questionnaire
+        </h1>
+        <p className="question-desc">
+            Please answer the following questions to help us recommend the best study methods and locations for you.
+        </p>
+        <div className="question-card">
+            <label className="question-label">
+            I am satisfied with my current study habits.
+            </label>
+            <div className="question-options">
+            {["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"].map((option) => (
+                <label key={option} className="question-option-label">
+                <input
+                    type="radio"
+                    name="studySatisfaction"
+                    value={option}
+                />
+                <div>{option}</div>
+                </label>
+            ))}
+            </div>
+        </div>
+        <div className="question-actions">
+            <Link to="/" className="question-link">
+            Back
+            </Link>
+            <button type="button" className="question-btn">
+            Next
+            </button>
+        </div>
+        </div>
+    );
+}
+
+export default QuestionPage;
