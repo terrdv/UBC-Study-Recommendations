c5cb1ef232c50900343daf7fefa6f8a24cdb3a1f
terrdv <terrdv7@gmail.com>
2025-09-27 23:42:11 -0700

    Implemented Backend to Frontend

--- client/src/pages/QuestionPage.jsx
+++ client/src/pages/QuestionPage.jsx
@@ -1,26 +1,37 @@
 import { Link } from "react-router-dom";
 import '../css/QuestionPage.css';
 import {useEffect, useState} from "react";
+import {fetchLocationQuestions} from "../services//api";
+
 
 
 
 function QuestionPage() {
-
     const [questions, setQuestions] = useState([]);
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [answers, setAnswers] = useState({});
 
+    
+    useEffect(() => {
+        const getQuestions = async () => {
+            const data = await fetchLocationQuestions();
+            setQuestions(data);
+        }
+        getQuestions();
+    }, []);
+
+
     return (
         <div className="question-bg">
         <h1 className="question-title">
-            Questionnaire
+            Question {currentQuestionIndex + 1}/{questions.length}
         </h1>
         <p className="question-desc">
             Please answer the following questions to help us recommend the best study methods and locations for you.
         </p>
         <div className="question-card">
             <label className="question-label">
-            I am satisfied with my current study habits.
+            {questions[currentQuestionIndex]?.text || "Loading..."}
             </label>
             <div className="question-options">
             {["Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"].map((option) => (
